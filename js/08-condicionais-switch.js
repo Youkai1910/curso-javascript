a17b9845d741c20ec193cbbbc871fb6d63adbbeb
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-04 16:46:30 -0300

    Exemplo da Condicional Switch

--- js/08-condicionais-switch.js
+++ js/08-condicionais-switch.js
@@ -1,1 +1,33 @@
 'use strict'
+
+/* "chatbot" */
+console.log("Bem-vindo ao ChatBot da Biribinha Infortima");
+console.log("\nComo posso ajudar? Escolha: \n\n" +
+    "1. Financeiro\n" +
+    "2. Vendas\n" +
+    "3. Suporte\n\n"
+);
+
+console.log("----------");
+
+let opcao = 1;
+
+switch (opcao) {
+    case 1:
+        console.log("Financeiro");
+        console.log("Legal, o que quer saber sobre finanças");
+        break;
+    case 2:
+        console.log("Vendas");
+        console.log("Show, quer comprar o que?");
+        break;
+    case 3:
+        console.log("Suporte");
+        console.log("Ok, o que está acontecendo?");
+        break;
+    default:
+        console.log("Não entendi");
+        console.log("Aguarde para falar com atendente humano...");
+}
+
+
