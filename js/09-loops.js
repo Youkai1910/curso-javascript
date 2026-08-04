701e8f48f7451df3aa959a3ceaf39d6b3744a24f
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-04 17:18:39 -0300

    Exemplo de loop while

--- js/09-loops.js
+++ js/09-loops.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// LOOP  WHILE (ENQUANTO)
+console.log("Exemplo 1: while()");
+console.log("Executa ações determinadas enquanto uma condição for verdadeira\n");
+
+/* Exibir o valor da variável de controle em um ciclo de 10vezes*/
+
+
+//Variável de controle
+let contador = 1;
+
+while (contador <= 10) {
+    console.log("Valor de contador é: "+contador);
+    ++contador
+}
+
