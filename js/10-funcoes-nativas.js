cc1eec94030f069a43509f7d10c1c56ceb5b1c77
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-05 17:05:29 -0300

    Exemplo Funções Nativas do JS

--- js/10-funcoes-nativas.js
+++ js/10-funcoes-nativas.js
@@ -3,3 +3,60 @@
 console.log("Funções para TEXTO\n\n");
 
 const frase = " JavaScript é INCRÍVEL! ";
+
+// Transformação em maiúsculas
+console.log(frase.toUpperCase());
+
+//Tansformação em minúsculas
+console.log(frase.toLowerCase());
+
+// verificar se um texto existe em outro
+console.log(frase.includes("Java")); // true
+console.log(frase.includes("JS")); //false
+
+// Procura e substitui
+console.log(frase.replace("INCRÍVEL", "Demais"));
+
+// Remoção de espaços no início e no fim
+console.log(frase.trim());
+
+console.log("--------------------------");
+
+console.log("Funções para NÚMERO\n\n");
+
+const preco = 19.95422;
+console.log(preco);
+
+//Definir quantidade de casas decimais 
+console.log(preco.toFixed(2));
+
+//obs.: tofixed()transforma o valor em string
+const precoComCasaDeCimal = preco.toFixed(2);
+console.log((precoComCasaDeCimal));
+
+//E Number()converte novamente pra número
+console.log(Number(precoComCasaDeCimal));
+
+console.log(typeof preco);//number
+console.log(typeof precoComCasaDeCimal);//string
+
+console.log(Math.round(preco));// arredonda
+console.log(Math.ceil(19.1));// arredonda para cima
+console.log(Math.floor(19.9));// arredonda pra baixo
+
+console.log(Math.max(10, 5, 200, 7));// retorna o maior
+console.log(Math.min(10, 5, 200, 7));// retorna o menor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
