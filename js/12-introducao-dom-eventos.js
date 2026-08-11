a2874c4792466440f4e15563757587f60ce58430
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-11 17:22:33 -0300

    Introdução Dom Eventos do JS

--- js/12-introducao-dom-eventos.js
+++ js/12-introducao-dom-eventos.js
@@ -1,1 +1,27 @@
-'use strict'
+'use strict';
+
+/* Exemplo 01 */
+
+//Busca e seleção de elementos através do ID
+ const pValor = document.querySelector("#valor");
+ const botaoSomar = document.querySelector("#somar");
+ const botaoSubtrair = document.querySelector("#subtrair");
+
+//Vriável para armazenar a contagem de cliques
+let contador = 0;
+
+// Desabilitando o botão subtrair
+botaoSubtrair.disabled = true;
+
+// Ouvinte de Evento para o botão de somar
+botaoSomar.addEventListener("click", function(){
+    //"Guardando" e somando cada clique
+    contador++;
+
+    //Exibindo o valor de contador no parágrafo selecionado
+    pValor.textContent = contador;
+
+    botaoSubtrair.disabled = false;
+});
+ 
+
