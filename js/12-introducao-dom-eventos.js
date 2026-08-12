3b870791e825cb8b6477824e00041ca8f2c2aae4
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-12 17:19:19 -0300

    exemplo 4 da introdução dom eventos do js

--- js/12-introducao-dom-eventos.js
+++ js/12-introducao-dom-eventos.js
@@ -3,9 +3,9 @@
 /* Exemplo 01 */
 
 //Busca e seleção de elementos através do ID
- const pValor = document.querySelector("#valor");
- const botaoSomar = document.querySelector("#somar");
- const botaoSubtrair = document.querySelector("#subtrair");
+const pValor = document.querySelector("#valor");
+const botaoSomar = document.querySelector("#somar");
+const botaoSubtrair = document.querySelector("#subtrair");
 
 //Vriável para armazenar a contagem de cliques
 let contador = 0;
@@ -14,7 +14,7 @@ let contador = 0;
 botaoSubtrair.disabled = true;
 
 // Ouvinte de Evento para o botão de somar
-botaoSomar.addEventListener("click", function(){
+botaoSomar.addEventListener("click", function () {
     //"Guardando" e somando cada clique
     contador++;
 
@@ -24,40 +24,92 @@ botaoSomar.addEventListener("click", function(){
     botaoSubtrair.disabled = false;
 });
 
-botaoSubtrair.addEventListener("click", function (){
+botaoSubtrair.addEventListener("click", function () {
     //Evitar valor negativo
-    if(contador > 0) contador--;
+    if (contador > 0) contador--;
 
     // Colocar no paragrafo chamado "valor" o número existente em contador
     pValor.textContent = contador;
 
     //Quando o contador for zero, desativamos o botão Subtrair
-    if( contador === 0) botaoSubtrair.disabled = true;
+    if (contador === 0) botaoSubtrair.disabled = true;
 });
 
 /* Exemplo 2 */
 const botaoMostrarEsconder = document.querySelector("#mostra-esconder");
-    const pTexto = document.querySelector("#texto");
+const pTexto = document.querySelector("#texto");
 
-  // Ouvinte de evento para botão
-  botaoMostrarEsconder.addEventListener("click", function(){
+// Ouvinte de evento para botão
+botaoMostrarEsconder.addEventListener("click", function () {
     //Usamos o toggle para ter o recurso de colocar/remover class
     pTexto.classList.toggle("oculto");
-// Alternando o texto o botanão
-/* se o texto do botão for esconder, troque para, Caso  contrário, coloque Esconder */
-if (botaoMostrarEsconder.textContent === "Esconder") {
-    botaoMostrarEsconder.textContent = "Mostrar";
-} else{
-    botaoMostrarEsconder.textContent = "Esconder";
-}
+    // Alternando o texto o botanão
+    /* se o texto do botão for esconder, troque para, Caso  contrário, coloque Esconder */
+    if (botaoMostrarEsconder.textContent === "Esconder") {
+        botaoMostrarEsconder.textContent = "Mostrar";
+    } else {
+        botaoMostrarEsconder.textContent = "Esconder";
+    }
 
-  });
+});
 
-  
 
+
+/* Exemplo 3 */
+// seleção por ID
+const imgFoto = document.querySelector("#foto");
+// seleção por ATRIBUTO
+const botoesDeFoto = document.querySelectorAll("[data-imagem]");
+
+
+
+//Percorrer/Acessat o array de botões
+for(const botao of botoesDeFoto){
+    botao.addEventListener("click", function(){
+        //Pegar /obter os dados  dos botões (imagem e o alt)
+        const imagem = botao.dataset.imagem;
+        const textoAlt = botao.dataset.alt;
+
+        //Aplicando a imagem e o texto alt a tag <img>
+        imgFoto.src = imagem;
+        imgFoto.alt = textoAlt;
+    });
+};
+/* Exemplo 4 */
+const textareaMensagem = document.querySelector("#mensagem");
+const spanContadorCaracteres = document.querySelector("#contador");
+const spanContadorRegressivo = document.querySelector("#regressivo");
+const blimite = document.querySelector("p small b");
+
+//Evento de entrada de dados (digitação no campo)
+textareaMensagem.addEventListener("input", function(){
+    //Capturando  durante a digitação  a qtd digitada de caracteres
+    let totalCaracteres = textareaMensagem.value.length;
+
+    // Exibindo o total de caracteres no spam
+    spanContadorCaracteres.textContent = totalCaracteres;
+
+    //contagem regressiva
+    const contagemRegressiva = 100 - totalCaracteres;
+    spanContadorRegressivo.textContent = contagemRegressiva;
+
+    if(totalCaracteres === 100){
+        spanContadorCaracteres.classList.add("excesso");
+        blimite.classList.add("excesso")
+    } else {
+        spanContadorRegressivo.classList.remove("excesso");
+    }
     
-    
- 
 
- 
+    });
+
+
+
+
+
+
+
+
+
+
 
