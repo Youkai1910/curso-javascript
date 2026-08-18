eb9e655172ba1762987f5e48d53031a0c84b775e
Youkai1910 <alexandre.gsantos26@senacsp.edu.br>
2026-08-18 17:06:58 -0300

    12-introducao-dom-eventos.html

--- js/12-introducao-dom-eventos.js
+++ js/12-introducao-dom-eventos.js
@@ -104,21 +104,86 @@ textareaMensagem.addEventListener("input", function () {
 const pTeclaPressionada = document.querySelector("#tecla b");
 const pagina = document.querySelector("body");
 
-document.addEventListener("keyup", function(event){
-    
-  const teclaUsada = event.key
+document.addEventListener("keyup", function (event) {
 
-  pTeclaPressionada.textContent = teclaUsada;
+    const teclaUsada = event.key
 
-  switch (teclaUsada) {
-    case "a": pagina.style.background = "yellow"; break;
-    case "b": pagina.style.background = "lightblue"; break;
-    case "c": pagina.style.background = "gray"; break; 
-    default: pagina.style.background = "red";
-  }
-  console.log(teclaUsada);
-  
+    pTeclaPressionada.textContent = teclaUsada;
+
+    switch (teclaUsada) {
+        case "a": pagina.style.background = "yellow"; break;
+        case "b": pagina.style.background = "lightblue"; break;
+        case "c": pagina.style.background = "gray"; break;
+        default: pagina.style.background = "red";
+    }
 });
+
+/* Exemplo 6 */
+
+// Aqui, primeiro selecionamos a div em que os elementos estão:
+const divTarefas = document.querySelector(".exemplo-tarefas");
+
+// Depois, fazemos o JS procurar DENTRO da div por cada elemento.
+// Isso torna a busca do JS mais rápida do que ao procurar no document.
+const inputTarefa = divTarefas.querySelector("#tarefa");
+const botaoAdicionar = divTarefas.querySelector("#adicionar");
+const listaTarefas = divTarefas.querySelector("#lista");
+
+botaoAdicionar.addEventListener("click", function () {
+    //Capturar o que foi digitado(valor do campo)
+    const textoTarefa = inputTarefa.value;
+
+    // Se o textoTarefa estiver vazio, interrompa as função
+    // fazemos isso com um simples "return".
+    if (textoTarefa === "") return;
+
+    // Criar o elemento/tag <li>
+    const item = document.createElement("li");
+
+    //Adicionar conteudo à nova tag criada
+    item.textContent = textoTarefa;
+
+
+    //Adicionar o elemento dentro da lista 
+    listaTarefas.appendChild(item);
+
+    inputTarefa.value = ""; // limpar o campo (deixar vazio)
+    inputTarefa.focus(); // devolve o foco para campo
+
+    //Detectar o clique em cada item e marca-lo como feito
+    item.addEventListener("click", function () {
+        item.classList.toggle("feito");
+    })
+
+});// final do evento / função do botão adicionar
+
+/* Mapeamento de Evento 
+Detectar o enter e mapear a execução  atrvés do clique*/
+inputTarefa.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") botaoAdicionar.click();
+
+});
+
+/* Exemplo 7 */
+const barraProgresso = document.querySelector("#barra");
+const botaoCarregar = document.querySelector("#carregar");
+const botaoLimpar = document.querySelector("#limpar");
+
+botaoCarregar.addEventListener("click", function () {
+    barraProgresso.value += 10;
+    if (barraProgresso.value === 100) barraProgresso.classList.add("concluido");
+});
+
+/* mini-exercicio: progrme o botão limpar para que, quando clicando, faça a barra de progresso voltar ao normal (sem cor e sem valor). */
+botaoLimpar.addEventListener("click", function(){
+   barraProgresso.value = 0 
+   barraProgresso.classList.remove("concluido");
+});
+
+/* Exemplo 8 */
+
+
+
 
 
 
